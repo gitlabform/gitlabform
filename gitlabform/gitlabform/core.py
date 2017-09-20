92334fe0f7b5b6088522f79dd2d68c4a892ce668
Greg Dubicki <gdubicki@egnyte.com>
2017-09-20 10:58:22 +0200

    Correct argparse helps

--- gitlabform/gitlabform/core.py
+++ gitlabform/gitlabform/core.py
@@ -89,7 +89,7 @@ class GitLabFormCore(object):
 
         group_ex.add_argument('-v', '--verbose', action="store_true", help='Verbose mode')
 
-        group_ex.add_argument('-d', '--debug', action="store_true", help='Verbose mode')
+        group_ex.add_argument('-d', '--debug', action="store_true", help='Debug mode (most verbose)')
 
         parser.add_argument('--strict', '-s', action="store_true", help='Stop on missing branches')
 
