3b645a5d3b818186ce2d9497d7f3b01b7587ab04
Greg Dubicki <gdubicki@egnyte.com>
2017-10-04 16:30:45 +0200

    Add tests showing that all config keys are merged in additive way...

--- gitlabform/configuration/test/test_projects_and_groups.py
+++ gitlabform/configuration/test/test_projects_and_groups.py
@@ -1,7 +1,8 @@
 import logging
 import os
-
+import pytest
 from gitlabform.configuration import ConfigurationProjectsAndGroups
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,3 +17,25 @@ class TestConfigurationProjectsAndGroups:
         x = c.get_effective_config_for_project('project_not_in_config/group_not_in_config')
 
         assert x == {}
+
+    def test__get_effective_config_for_project__only_group_and_project__project_from_config__additive_project_settings(self):
+
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        c = ConfigurationProjectsAndGroups(os.path.join(current_path, 'config_with_only_group_and_project.yaml'))
+
+        x = c.get_effective_config_for_project('some_group/some_project')
+
+        additive__project_settings = x['project_settings']
+
+        # merged hashes from group and project levels
+        assert additive__project_settings == {'foo': 'bar', 'bar': 'foo'}
+
+    def test__get_effective_config_for_project__only_group_and_project__project_from_config__additive_hooks(self):
+
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        c = ConfigurationProjectsAndGroups(os.path.join(current_path, 'config_with_only_group_and_project.yaml'))
+
+        x = c.get_effective_config_for_project('some_group/some_project')
+
+        additive__hooks = x['hooks']
+        assert additive__hooks == {'a': {'foo': 'bar'}, 'b': {'bar': 'foo'}}  # added from both group and project level
