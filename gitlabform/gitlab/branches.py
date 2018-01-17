b89ebe828cf77ca5ddd012a48170544adfe76a49
Greg Dubicki <gdubicki@egnyte.com>
2018-01-17 19:03:28 +0100

    Add some methods for internal use

--- gitlabform/gitlab/branches.py
+++ gitlabform/gitlab/branches.py
@@ -33,3 +33,33 @@ class GitLabBranches(GitLabCore):
     def get_branch(self, project_and_group_name, branch):
         pid = self._get_project_id(project_and_group_name)
         return self._make_requests_to_api("projects/%s/repository/branches/%s", (pid, branch))
+
+    def delete_branch(self, group_and_project_name, branch):
+        pid = self._get_project_id(group_and_project_name)
+        self._make_requests_to_api("projects/%s/repository/branches/%s", (pid, branch), method='DELETE')
+
+    def get_protected_branches(self, project_and_group_name):
+        pid = self._get_project_id(project_and_group_name)
+
+        branches = self._make_requests_to_api("projects/%s/repository/branches", pid)
+
+        protected_branches = []
+        for branch in branches:
+            if branch['protected']:
+                name = branch['name']
+                protected_branches.append(name)
+
+        return protected_branches
+
+    def get_unprotected_branches(self, project_and_group_name):
+        pid = self._get_project_id(project_and_group_name)
+
+        branches = self._make_requests_to_api("projects/%s/repository/branches", pid)
+
+        feature_branches = []
+        for branch in branches:
+            if not branch['protected']:
+                name = branch['name']
+                feature_branches.append(name)
+
+        return feature_branches
--- gitlabform/gitlab/commits.py
+++ gitlabform/gitlab/commits.py
@@ -6,3 +6,35 @@ class GitLabCommits(GitLabCore):
     def get_commit(self, project_and_group_name, sha):
         pid = self._get_project_id(project_and_group_name)
         return self._make_requests_to_api("projects/%s/repository/commits/%s", (pid, sha))
+
+    def get_ahead_and_behind(self, project_and_group_name, protected_branch, feature_branch):
+        pid = self._get_project_id(project_and_group_name)
+
+        ahead = 0
+        behind = 0
+
+        response = self._make_requests_to_api("projects/%s/repository/compare?from=%s&to=%s",
+                                              (pid, protected_branch, feature_branch))
+        if len(response) > 0:
+            ahead = len(response['commits'])
+
+        response = self._make_requests_to_api("projects/%s/repository/compare?from=%s&to=%s",
+                                              (pid, feature_branch, protected_branch))
+        if len(response) > 0:
+            behind = len(response['commits'])
+
+        return ahead, behind
+
+    def get_last_commit_attributes(self, project_and_group_name, branch):
+        pid = self._get_project_id(project_and_group_name)
+
+        branch = self._make_requests_to_api("projects/%s/repository/branches/%s", (pid, branch))
+
+        last_commit_hash = branch['commit']['id']
+
+        commit = self._make_requests_to_api("projects/%s/repository/commits/%s", (pid, last_commit_hash))
+
+        # we want to read git's *commit date* instead of *author date*, so that "touching" the branch will invalidate
+        # its "getting older" counter
+
+        return commit['author_name'], commit['author_email'], commit['committed_date']
