784f2e06c1a7ab6670d7aa4e509369e2c978d5d2
Greg Dubicki <gdubicki@egnyte.com>
2018-08-04 18:30:55 +0200

    Update to new Repository files API in v4

--- gitlabform/gitlab/repositories.py
+++ gitlabform/gitlab/repositories.py
@@ -16,37 +16,37 @@ class GitLabRepositories(GitLabCore):
 
     def get_file(self, project_and_group_name, branch, path):
         pid = self._get_project_id(project_and_group_name)
-        result = self._make_requests_to_api("projects/%s/repository/files?ref=%s&file_path=%s", (pid, branch, path))
+        result = self._make_requests_to_api("projects/%s/repository/files/%s?ref=%s", (pid, path, branch))
         return base64.b64decode(result['content']).decode("utf-8")
 
     def set_file(self, project_and_group_name, branch, path, content, commit_message):
         data = {
             "branch": branch,
-            "path": path,
+            "file_path": path,
             "content": content,
             "commit_message": commit_message
         }
         pid = self._get_project_id(project_and_group_name)
-        return self._make_requests_to_api("projects/%s/repository/files?ref=%s&file_path=%s", (pid, branch, path),
+        return self._make_requests_to_api("projects/%s/repository/files/%s", (pid, path),
                                           'PUT', data=data)
 
     def add_file(self, project_and_group_name, branch, path, content, commit_message):
         data = {
             "branch": branch,
-            "path": path,
+            "file_path": path,
             "content": content,
             "commit_message": commit_message
         }
         pid = self._get_project_id(project_and_group_name)
-        return self._make_requests_to_api("projects/%s/repository/files?ref=%s&file_path=%s", (pid, branch, path),
+        return self._make_requests_to_api("projects/%s/repository/files/%s", (pid, path),
                                           'POST', data=data, expected_codes=201)
 
     def delete_file(self, project_and_group_name, branch, path, commit_message):
         data = {
             "branch": branch,
-            "path": path,
+            "file_path": path,
             "commit_message": commit_message
         }
         pid = self._get_project_id(project_and_group_name)
-        return self._make_requests_to_api("projects/%s/repository/files?ref=%s&file_path=%s", (pid, branch, path),
+        return self._make_requests_to_api("projects/%s/repository/files/%s", (pid, path),
                                           'DELETE', data=data)
