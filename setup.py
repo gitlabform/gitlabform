860a96c7ca34c020fef4002c6fb37e558c677f03
bjarocki <bjarocki@egnyte.com>
2018-09-28 12:50:14 +0200

    Release v 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def convert_markdown_to_rst(file):
 
 
 setup(name='gitlabform',
-      version='1.0.0',
+      version='1.0.1',
       description='Easy configuration as code tool for GitLab using config in plain YAML',
       long_description=convert_markdown_to_rst('README.md'),
       url='https://github.com/egnyte/gitlabform',
