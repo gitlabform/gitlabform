6cbab587d08e87e9a09a39aa3e979cc460cd8a20
bjarocki <bjarocki@egnyte.com>
2018-12-12 13:27:03 +0100

    OPS-30457 Pipelines & retry if failed

--- gitlabform/gitlab/pipelines.py
+++ gitlabform/gitlab/pipelines.py
@@ -4,43 +4,20 @@ from datetime import *
 
 
 class GitLabPipelines(GitLabCore):
-    SDDC_ENVS = '441'
-    BRANCHES = ['egnyte', 'global_production', 'global_qa', 'internal', 'partners', 'perf2', 'production',
-                    'qa', 'qabranchprod', 'qabranchrc']
-
-    def check_envs(self):
+    def check_envs_and_retry(self, sddc_envs_id, branches):
+        print('Checking ENVS & retry if failed')
         for i in range(3):
             sleep = 10
             print('Current time %s, waiting %ss.' % (datetime.now(), sleep))
             t.sleep(sleep)
             print('Starts at %s.' % (datetime.now()))
-            for branch in self.BRANCHES:
-                pipelines = self._make_requests_to_api("projects/%s/pipelines?ref=%s", (self.SDDC_ENVS, branch))
+            for branch in branches:
+                pipelines = self._make_requests_to_api("projects/%s/pipelines?ref=%s", (sddc_envs_id, branch))
                 last_pipeline = pipelines[0]
                 if last_pipeline['status'] == 'failed' or last_pipeline['status'] == 'pending':
                     print('Branch: %s - status: %s, id: %s\nweb_url: %s' %
                           (branch, last_pipeline['status'], last_pipeline['id'], last_pipeline['web_url']))
                     retry_pipeline = self._make_requests_to_api("projects/%s/pipelines/%s/retry",
-                                                               (self.SDDC_ENVS, last_pipeline['id']),
+                                                               (sddc_envs_id, last_pipeline['id']),
                                                                method='POST',expected_codes=[200, 201])
 
-
-    def get_project_id(self, module):
-        name = (module.split('/'))[1]
-        project = self._make_requests_to_api("projects?search=%s", name)
-        print(project[0])
-        return project[0]['id']
-
-
-    def test(self, modules_and_new_tags):
-        print('test')
-        for module_and_tag in modules_and_new_tags:
-            module, tag = module_and_tag
-            id = self.get_project_id((module))
-            print(id)
-            test = self._make_requests_to_api("projects/%s/repository/commits", str(id))
-            print(test)
-            print('----------------------------------------')
-            print(test[0])
-            single_commit = self._make_requests_to_api("projects/%s/repository/commits/%s", (str(id), test[0]['id']))
-            print(single_commit)
