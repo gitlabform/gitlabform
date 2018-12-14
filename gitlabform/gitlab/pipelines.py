b50dc35677b13285c84d98b6c394166212a53941
bjarocki <bjarocki@egnyte.com>
2018-12-14 12:47:35 +0100

    OPS-30457 pipelines

--- gitlabform/gitlab/pipelines.py
+++ gitlabform/gitlab/pipelines.py
@@ -1,23 +1,14 @@
 from gitlabform.gitlab.core import GitLabCore
-import time as t
-from datetime import *
 
 
 class GitLabPipelines(GitLabCore):
-    def check_envs_and_retry(self, sddc_envs_id, branches):
-        print('Checking ENVS & retry if failed')
-        for i in range(3):
-            sleep = 10
-            print('Current time %s, waiting %ss.' % (datetime.now(), sleep))
-            t.sleep(sleep)
-            print('Starts at %s.' % (datetime.now()))
-            for branch in branches:
-                pipelines = self._make_requests_to_api("projects/%s/pipelines?ref=%s", (sddc_envs_id, branch))
-                last_pipeline = pipelines[0]
-                if last_pipeline['status'] == 'failed' or last_pipeline['status'] == 'pending':
-                    print('Branch: %s - status: %s, id: %s\nweb_url: %s' %
-                          (branch, last_pipeline['status'], last_pipeline['id'], last_pipeline['web_url']))
-                    retry_pipeline = self._make_requests_to_api("projects/%s/pipelines/%s/retry",
-                                                               (sddc_envs_id, last_pipeline['id']),
+
+    def get_pipelines(self, project_and_group_name, branch):
+        pipelines = self._make_requests_to_api("projects/%s/pipelines?ref=%s", (project_and_group_name, branch))
+        return pipelines
+
+    def retry_pipeline(self, project_and_group_name, pipeline_id):
+        pipeline = self._make_requests_to_api("projects/%s/pipelines/%s/retry",
+                                                               (project_and_group_name, pipeline_id),
                                                                method='POST',expected_codes=[200, 201])
 
