768b6614ff311803ca98c96f5e36a64b49b7dc2b
Greg Dubicki <gdubicki@egnyte.com>
2019-01-21 10:43:59 +0100

    Fix style issues

--- gitlabform/gitlab/pipelines.py
+++ gitlabform/gitlab/pipelines.py
@@ -5,9 +5,8 @@ class GitLabPipelines(GitLabCore):
 
     def get_pipelines(self, project_and_group_name, branch):
         pipelines = self._make_requests_to_api("projects/%s/pipelines?ref=%s", (project_and_group_name, branch),
-                                                paginated = True)
+                                               paginated=True)
         return pipelines
-
 
     def get_pipeline(self, project_and_group_name, pipeline_id):
         pipeline = self._make_requests_to_api("/projects/%s/pipelines/%s", (project_and_group_name, pipeline_id))
@@ -15,7 +14,7 @@ class GitLabPipelines(GitLabCore):
 
     def retry_pipeline(self, project_and_group_name, pipeline_id):
         pipeline = self._make_requests_to_api("projects/%s/pipelines/%s/retry",
-                                                               (project_and_group_name, pipeline_id),
-                                                               method='POST',expected_codes=[200, 201])
+                                              (project_and_group_name, pipeline_id),
+                                              method='POST', expected_codes=[200, 201])
         return pipeline
 
