c5b9b16fb8a44d937ae70a59adc8b3f1e34ea068
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2019-06-09 11:33:47 +0200

    run docker build without push for non-tags pipelines

--- docker.sh
+++ docker.sh
@@ -5,24 +5,31 @@ for os in alpine debian
 do
     effective_version="${os}_version"
     suffix="${os}${!effective_version}"
+    latest="egnyte/gitlabform:latest-${suffix}"
+    docker pull "${latest}" || echo "no cche is availkable"
     docker build \
         --build-arg PY_VERSION=$python_version \
         --build-arg OS_VERSION="${!effective_version}" \
         --file "${os}.Dockerfile" \
-        --tag "egnyte/gitlabform:latest-${suffix}" .
-    tags="$tags egnyte/gitlabform:latest-${suffix}"
+        --tag "${latest}" .
+    tags="$tags ${latest}"
 
-    docker tag "egnyte/gitlabform:latest-${suffix}" "egnyte/gitlabform:${version}-${suffix}"
+    docker tag "${latest}" "egnyte/gitlabform:${version}-${suffix}"
     tags="$tags !$"
 
     if [ "$os" = "alpine" ]
     then
-        docker tag "egnyte/gitlabform:latest-${suffix}" "egnyte/gitlabform:latest"
+        docker tag "${latest}" "egnyte/gitlabform:latest"
         tags="$tags !$"
     fi
 done
 
 for image in $tags
 do
-    docker push $image
+    if [ ! -z "${PUSH_IMAGES}" ]
+    then
+            docker push $image
+    else
+        echo "$image"
+    fi
 done
