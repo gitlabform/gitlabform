6d411359e21e57ddb2570427d6afc4f97c7e286e
Barnabas Kutassy <barnabas.kutassy@mollie.com>
2019-12-20 16:32:24 +0100

    Update to use unicode emoji

--- gitlabform/gitlab/members.py
+++ gitlabform/gitlab/members.py
@@ -11,7 +11,7 @@ class GitLabMembers(GitLabCore):
         }
         if access_level is not None:
             data['access_level'] = access_level
-        logging.warning("\t❇️  ++ ADD user access: %s [%s] to: %s", user, access_level, project_and_group_name)
+        logging.warning("\t✅ ++ ADD user access: %s [%s] to: %s", user, access_level, project_and_group_name)
         return self._make_requests_to_api("projects/%s/members", project_and_group_name, method='POST',
                                          data=data, expected_codes=201)
 
@@ -23,7 +23,7 @@ class GitLabMembers(GitLabCore):
         }
         if access_level is not None:
             data['access_level'] = access_level
-        logging.warning("\t⚠️  ~~ UPD user access: %s [%s] to: %s", user, access_level, group_and_project_name)
+        logging.warning("\t📒 ~~ MOD user access: %s [%s] to: %s", user, access_level, group_and_project_name)
         return self._make_requests_to_api("projects/%s/members/%s", (group_and_project_name, user_id), method="PUT",
                                          data=data, expected_codes=200)
 
@@ -39,7 +39,7 @@ class GitLabMembers(GitLabCore):
         }
         if access_level is not None:
             data['access_level'] = access_level
-        logging.warning("\t❇️  ++ ADD user membership to group: %s [%s] to: %s", user, access_level, group_name)
+        logging.warning("\t✅ ++ ADD user membership to group: %s [%s] to: %s", user, access_level, group_name)
         return self._make_requests_to_api("groups/%s/members", group_name, method='POST',
                                          data=data, expected_codes=201)
 
@@ -51,7 +51,7 @@ class GitLabMembers(GitLabCore):
         }
         if access_level is not None:
             data['access_level'] = access_level
-        logging.warning("\t⚠️  ~~ UPD user membership to group: %s [%s] to: %s", user, access_level, group_name)
+        logging.warning("\t📒 ~~ MOD user membership to group: %s [%s] to: %s", user, access_level, group_name)
         return self._make_requests_to_api("groups/%s/members/%s", (group_name, user_id), method="PUT",
                                          data=data, expected_codes=200)
 
--- gitlabform/gitlab/projects.py
+++ gitlabform/gitlab/projects.py
@@ -201,7 +201,7 @@ class GitLabProjects(GitLabCore):
         }
         if group_access is not None:
             data['group_access'] = group_access
-        logging.warning("\t❇️  ++ ADD group access: %s [%s] to: %s", group_name, group_access, project_and_group_name)
+        logging.warning("\t✅ ++ ADD group access: %s [%s] to: %s", group_name, group_access, project_and_group_name)
         return self._make_requests_to_api("projects/%s/share", project_and_group_name, method='POST', data=data, expected_codes=201)
 
     def update_group_share_of_project(self, project_and_group_name, group_name, group_access, expires_at):
@@ -212,7 +212,7 @@ class GitLabProjects(GitLabCore):
         if group_access is not None:
             data['group_access'] = group_access
         group_id = self._get_group_id(group_name)
-        logging.warning("\t⚠️  ~~ UPD group access: %s [%s] to: %s", group_name, group_access, project_and_group_name)
+        logging.warning("\t📒 ~~ MOD group access: %s [%s] to: %s", group_name, group_access, project_and_group_name)
         return self._make_requests_to_api("projects/%s/share/%s", (project_and_group_name, group_id), method='PUT', data=data, expected_codes=200)
 
     def unshare_with_group(self, project_and_group_name, group_name):
