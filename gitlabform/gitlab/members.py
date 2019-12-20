b4bee4d1910551a3dd75c73e5fbfff705ba63cfd
Barnabas Kutassy <barnabas.kutassy@mollie.com>
2019-12-20 16:25:27 +0100

    Cleanup and proper emojis

--- gitlabform/gitlab/members.py
+++ gitlabform/gitlab/members.py
@@ -11,7 +11,7 @@ class GitLabMembers(GitLabCore):
         }
         if access_level is not None:
             data['access_level'] = access_level
-        logging.warning("\t++ ADD user access: %s [%s] to: %s", user, access_level, project_and_group_name)
+        logging.warning("\t❇️  ++ ADD user access: %s [%s] to: %s", user, access_level, project_and_group_name)
         return self._make_requests_to_api("projects/%s/members", project_and_group_name, method='POST',
                                          data=data, expected_codes=201)
 
@@ -23,12 +23,12 @@ class GitLabMembers(GitLabCore):
         }
         if access_level is not None:
             data['access_level'] = access_level
-        logging.warning("\t~~ UPD user access: %s [%s] to: %s", user, access_level, group_and_project_name)
+        logging.warning("\t⚠️  ~~ UPD user access: %s [%s] to: %s", user, access_level, group_and_project_name)
         return self._make_requests_to_api("projects/%s/members/%s", (group_and_project_name, user_id), method="PUT",
                                          data=data, expected_codes=200)
 
     def remove_member_from_project(self, project_and_group_name, user):
-        logging.warning("\t-- DEL user access: %s to: %s", user, project_and_group_name)
+        logging.warning("\t🚫 -- DEL user access: %s to: %s", user, project_and_group_name)
         return self._make_requests_to_api("projects/%s/members/%s", (project_and_group_name, self._get_user_id(user)),
                                           method='DELETE', expected_codes=204)
 
@@ -39,7 +39,7 @@ class GitLabMembers(GitLabCore):
         }
         if access_level is not None:
             data['access_level'] = access_level
-        logging.warning("\t++ ADD user membership to group: %s [%s] to: %s", user, access_level, group_name)
+        logging.warning("\t❇️  ++ ADD user membership to group: %s [%s] to: %s", user, access_level, group_name)
         return self._make_requests_to_api("groups/%s/members", group_name, method='POST',
                                          data=data, expected_codes=201)
 
@@ -51,23 +51,11 @@ class GitLabMembers(GitLabCore):
         }
         if access_level is not None:
             data['access_level'] = access_level
-        logging.warning("\t~~ UPD user membership to group: %s [%s] to: %s", user, access_level, group_name)
+        logging.warning("\t⚠️  ~~ UPD user membership to group: %s [%s] to: %s", user, access_level, group_name)
         return self._make_requests_to_api("groups/%s/members/%s", (group_name, user_id), method="PUT",
                                          data=data, expected_codes=200)
 
     def remove_member_from_group(self, group_name, user):
-        logging.warning("\t-- DEL user membership to group: %s [%s] to: %s", user, group_name)
+        logging.warning("\t🚫 -- DEL user membership to group: %s [%s] to: %s", user, group_name)
         self._make_requests_to_api("groups/%s/members/%s", (group_name, self._get_user_id(user)),
                                         method='DELETE', expected_codes=204)
-
-
-    # def update_group_share_of_project(self, group_name, user, access_level, expires_at):
-    #     user_id =self._get_user_id(user)
-    #     data = {
-    #         "user_id": user_id,
-    #         "expires_at": expires_at
-    #     }
-    #     if access_level is not None:
-    #         data['access_level'] = access_level
-    #     return self._make_requests_to_api("groups/%s/members/%s", (group_name, user_id), method="PUT",
-    #                                      data=data, expected_codes=200)
