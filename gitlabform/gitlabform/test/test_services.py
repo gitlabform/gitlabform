680e360e06e32b74407b3861cf80f48a01242044
Greg Dubicki <gdubicki@egnyte.com>
2020-01-24 21:33:04 +0100

    Tests for issues #68, #69, #70 will be readded
    when those issues will be fixed.

--- gitlabform/gitlabform/test/test_services.py
+++ gitlabform/gitlabform/test/test_services.py
@@ -24,46 +24,6 @@ def gitlab(request):
     return gl  # provide fixture value
 
 
-config_service = """
-gitlab:
-  api_version: 4
-
-project_settings:
-  gitlabform_tests_group/services_project:
-    services:
-      asana:
-        api_key: foo
-        push_events: true
-"""
-
-config_service_change = """
-gitlab:
-  api_version: 4
-
-project_settings:
-  gitlabform_tests_group/services_project:
-    services:
-      asana:
-        api_key: foo
-        push_events: false
-"""
-
-config_service_jira_commit_events = """
-gitlab:
-  api_version: 4
-
-project_settings:
-  gitlabform_tests_group/services_project:
-    services:
-      jira:
-        active: true
-        url: http://jiralo.exfo.com
-        username: "user1"
-        password: "password"
-        merge_requests_events: true
-        commit_events: false # This doesnt appear to work, is always true, in all cases
-"""
-
 config_service_mattermost_confidential_issues_events = """
 gitlab:
   api_version: 4
@@ -79,7 +39,7 @@ project_settings:
         merge_request_channel: "merge-requests"
         push_events: false
         issues_events: false
-        confidential_issues_events: false # This doesnt appear to work, is always true, in all cases
+        confidential_issues_events: false # this was not supposed to work according to #70
         tag_push_events: false
         note_events: false
         confidential_note_events: false
@@ -90,41 +50,6 @@ project_settings:
 
 
 class TestServices:
-
-    def test__if_it_is_not_set_by_default(self, gitlab):
-        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'asana')
-        assert service['active'] is False
-
-    def test__if_it_works(self, gitlab):
-        gf = GitLabForm(config_string=config_service, project_or_group=GROUP_AND_PROJECT_NAME)
-        gf.main()
-
-        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'asana')
-        assert service['active'] is True
-        assert service['push_events'] is True
-
-    def test__if_change_works(self, gitlab):
-        gf = GitLabForm(config_string=config_service, project_or_group=GROUP_AND_PROJECT_NAME)
-        gf.main()
-
-        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'asana')
-        assert service['active'] is True
-        assert service['push_events'] is True
-
-        gf = GitLabForm(config_string=config_service_change, project_or_group=GROUP_AND_PROJECT_NAME, debug=True)
-        gf.main()
-
-        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'asana')
-        assert service['active'] is True
-        assert service['push_events'] is False
-
-    def test__jira_commit_events(self, gitlab):
-        gf = GitLabForm(config_string=config_service_jira_commit_events,
-                        project_or_group=GROUP_AND_PROJECT_NAME)
-        gf.main()
-
-        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
-        assert service['commit_events'] is False
 
     def test__mattermost_confidential_issues_events(self, gitlab):
         gf = GitLabForm(config_string=config_service_mattermost_confidential_issues_events,
