7354bc8f5c86a39c236851265ea2a83595c1f3d5
Greg Dubicki <gdubicki@egnyte.com>
2020-03-07 12:29:32 +0100

    Uncomment mattermost test

--- gitlabform/gitlabform/test/test_services.py
+++ gitlabform/gitlabform/test/test_services.py
@@ -18,6 +18,7 @@ def gitlab(request):
     def fin():
         # disable test integrations
         gl.delete_service(GROUP_AND_PROJECT_NAME, 'jira')
+        gl.delete_service(GROUP_AND_PROJECT_NAME, 'mattermost')
 
     request.addfinalizer(fin)
     return gl  # provide fixture value
@@ -51,35 +52,35 @@ project_settings:
         commit_events: true
 """
 
-# config_service_mattermost_confidential_issues_events = """
-# gitlab:
-#   api_version: 4
-#
-# project_settings:
-#   gitlabform_tests_group/services_project:
-#     services:
-#       mattermost:
-#         active: true
-#         webhook: https://mattermost.com/hooks/xxx
-#         username: gitlab
-#         merge_requests_events: true
-#         merge_request_channel: "merge-requests"
-#         push_events: false
-#         issues_events: false
-#         confidential_issues_events: false # this was not supposed to work according to #70
-#         tag_push_events: false
-#         note_events: false
-#         confidential_note_events: false
-#         pipeline_events: false
-#         wiki_page_events: false
-#         branches_to_be_notified: "all"
-# """
+config_service_mattermost_confidential_issues_events = """
+gitlab:
+  api_version: 4
+
+project_settings:
+  gitlabform_tests_group/services_project:
+    services:
+      mattermost:
+        active: true
+        webhook: https://mattermost.com/hooks/xxx
+        username: gitlab
+        merge_requests_events: true
+        merge_request_channel: "merge-requests"
+        push_events: false
+        issues_events: false
+        confidential_issues_events: false # this was not supposed to work according to #70
+        tag_push_events: false
+        note_events: false
+        confidential_note_events: false
+        pipeline_events: false
+        wiki_page_events: false
+        branches_to_be_notified: "all"
+"""
 
 
 class TestServices:
 
     def test__if_it_is_not_set_by_default(self, gitlab):
-        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'asana')
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
         assert service['active'] is False
 
     def test__if_jira_commit_events_true_works(self, gitlab: GitLab):
@@ -109,10 +110,10 @@ class TestServices:
     #     service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
     #     assert service['commit_events'] is False
 
-    # def test__mattermost_confidential_issues_events(self, gitlab: GitLab):
-    #     gf = GitLabForm(config_string=config_service_mattermost_confidential_issues_events,
-    #                     project_or_group=GROUP_AND_PROJECT_NAME)
-    #     gf.main()
-    #
-    #     service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'mattermost')
-    #     assert service['confidential_issues_events'] is False
+    def test__mattermost_confidential_issues_events(self, gitlab: GitLab):
+        gf = GitLabForm(config_string=config_service_mattermost_confidential_issues_events,
+                        project_or_group=GROUP_AND_PROJECT_NAME)
+        gf.main()
+
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'mattermost')
+        assert service['confidential_issues_events'] is False
