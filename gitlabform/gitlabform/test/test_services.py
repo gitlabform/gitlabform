90f859222ea356d22f8b3366eadefb3e74e781fb
Greg Dubicki <gdubicki@egnyte.com>
2020-03-09 19:39:26 +0100

    Merge all kinds of service tests

--- gitlabform/gitlabform/test/test_services.py
+++ gitlabform/gitlabform/test/test_services.py
@@ -17,10 +17,8 @@ def gitlab(request):
 
     def fin():
         # disable test integrations
-        gl.delete_service(GROUP_AND_PROJECT_NAME, 'mattermost')
-        gl.delete_service(GROUP_AND_PROJECT_NAME, 'asana')
-        gl.delete_service(GROUP_AND_PROJECT_NAME, 'hipchat')
-        gl.delete_service(GROUP_AND_PROJECT_NAME, 'redmine')
+        for service in ['asana', 'hipchat', 'redmine', 'jira', 'mattermost']:
+            gl.delete_service(GROUP_AND_PROJECT_NAME, service)
 
     request.addfinalizer(fin)
     return gl  # provide fixture value
@@ -64,36 +62,79 @@ project_settings:
         push_events: false # changed
 """
 
-# config_service_mattermost_confidential_issues_events = """
-# gitlab:
-#   api_version: 4
-#
-# project_settings:
-#   gitlabform_tests_group/services_project:
-#     services:
-#       mattermost:
-#         active: true
-#         webhook: https://mattermost.com/hooks/xxx
-#         username: gitlab
-#         merge_requests_events: true
-#         merge_request_channel: "merge-requests"
-#         push_events: false
-#         issues_events: false
-#         confidential_issues_events: false # this was not supposed to work according to #70
-#         tag_push_events: false
-#         note_events: false
-#         confidential_note_events: false
-#         pipeline_events: false
-#         wiki_page_events: false
-#         branches_to_be_notified: "all"
-# """
+config_service_jira_commit_events_true = """
+gitlab:
+  api_version: 4
+project_settings:
+  gitlabform_tests_group/services_project:
+    services:
+      jira:
+        url: http://foo.bar.com
+        username: foo
+        password: bar
+        active: true
+        commit_events: true
+"""
+
+config_service_jira_commit_events_false = """
+gitlab:
+  api_version: 4
+project_settings:
+  gitlabform_tests_group/services_project:
+    services:
+      jira:
+        url: http://foo.bar.com
+        username: foo
+        password: bar
+        active: true
+        commit_events: false
+"""
+
+config_service_jira_delete = """
+gitlab:
+  api_version: 4
+project_settings:
+  gitlabform_tests_group/services_project:
+    services:
+      jira:
+        delete: true
+"""
+
+config_service_mattermost_confidential_issues_events = """
+gitlab:
+  api_version: 4
+
+project_settings:
+  gitlabform_tests_group/services_project:
+    services:
+      mattermost:
+        active: true
+        webhook: https://mattermost.com/hooks/xxx
+        username: gitlab
+        merge_requests_events: true
+        merge_request_channel: "merge-requests"
+        push_events: false
+        issues_events: false
+        confidential_issues_events: false # this was not supposed to work according to #70
+        tag_push_events: false
+        note_events: false
+        confidential_note_events: false
+        pipeline_events: false
+        wiki_page_events: false
+        branches_to_be_notified: "all"
+"""
 
 
 class TestServices:
 
-    def test__if_it_is_not_set_by_default(self, gitlab):
-        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'asana')
-        assert service['active'] is False
+    def test__if_they_are_not_set_by_default(self, gitlab):
+
+        services = []
+        for service_name in ['asana', 'hipchat', 'redmine']:
+            service = gitlab.get_service(GROUP_AND_PROJECT_NAME, service_name)
+            services.append(service)
+
+        assert not any([service['active'] for service in services]) is True
 
     def test__if_push_events_true_works(self, gitlab: GitLab):
         gf = GitLabForm(config_string=config_service_push_events_true, project_or_group=GROUP_AND_PROJECT_NAME)
@@ -105,7 +146,7 @@ class TestServices:
             services.append(service)
 
         assert all([service['active'] for service in services]) is True
-        assert all([service['push_events'] for service in services])  is True
+        assert all([service['push_events'] for service in services]) is True
 
     def test__if_push_events_false_works(self, gitlab: GitLab):
         gf = GitLabForm(config_string=config_service_push_events_false, project_or_group=GROUP_AND_PROJECT_NAME)
@@ -119,7 +160,7 @@ class TestServices:
         assert all([service['active'] for service in services]) is True
         assert all([service['push_events'] for service in services]) is False
 
-    def test__if_change_works(self, gitlab: GitLab):
+    def test__if_push_events_change_works(self, gitlab: GitLab):
         gf = GitLabForm(config_string=config_service_push_events_true, project_or_group=GROUP_AND_PROJECT_NAME)
         gf.main()
 
@@ -129,7 +170,7 @@ class TestServices:
             services.append(service)
 
         assert all([service['active'] for service in services]) is True
-        assert all([service['push_events'] for service in services])  is True
+        assert all([service['push_events'] for service in services]) is True
 
         gf = GitLabForm(config_string=config_service_push_events_false, project_or_group=GROUP_AND_PROJECT_NAME)
         gf.main()
@@ -142,10 +183,59 @@ class TestServices:
         assert all([service['active'] for service in services]) is True
         assert all([service['push_events'] for service in services]) is False
 
-    # def test__mattermost_confidential_issues_events(self, gitlab: GitLab):
-    #     gf = GitLabForm(config_string=config_service_mattermost_confidential_issues_events,
-    #                     project_or_group=GROUP_AND_PROJECT_NAME)
-    #     gf.main()
-    #
-    #     service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'mattermost')
-    #     assert service['confidential_issues_events'] is False
+    def test__if_jira_is_not_active_by_default(self, gitlab):
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
+        assert service['active'] is False
+
+    def test__if_jira_commit_events_true_works(self, gitlab: GitLab):
+        gf = GitLabForm(config_string=config_service_jira_commit_events_true, project_or_group=GROUP_AND_PROJECT_NAME)
+        gf.main()
+
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
+        assert service['active'] is True
+        assert service['commit_events'] is True
+
+    def test__if_jira_commit_events_false_works(self, gitlab: GitLab):
+        gf = GitLabForm(config_string=config_service_jira_commit_events_false, project_or_group=GROUP_AND_PROJECT_NAME)
+        gf.main()
+
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
+        assert service['active'] is True
+        assert service['commit_events'] is False
+
+    def test__if_change_works(self, gitlab: GitLab):
+        gf = GitLabForm(config_string=config_service_jira_commit_events_true, project_or_group=GROUP_AND_PROJECT_NAME)
+        gf.main()
+
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
+        assert service['active'] is True
+        assert service['commit_events'] is True
+
+        gf = GitLabForm(config_string=config_service_jira_commit_events_false, project_or_group=GROUP_AND_PROJECT_NAME)
+        gf.main()
+
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
+        assert service['active'] is True
+        assert service['commit_events'] is False
+
+    def test__if_delete_works(self, gitlab: GitLab):
+        gf = GitLabForm(config_string=config_service_jira_commit_events_true, project_or_group=GROUP_AND_PROJECT_NAME)
+        gf.main()
+
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
+        assert service['active'] is True
+        assert service['commit_events'] is True
+
+        gf = GitLabForm(config_string=config_service_jira_delete, project_or_group=GROUP_AND_PROJECT_NAME)
+        gf.main()
+
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'jira')
+        assert service['active'] is False
+
+    def test__mattermost_confidential_issues_events(self, gitlab: GitLab):
+        gf = GitLabForm(config_string=config_service_mattermost_confidential_issues_events,
+                        project_or_group=GROUP_AND_PROJECT_NAME)
+        gf.main()
+
+        service = gitlab.get_service(GROUP_AND_PROJECT_NAME, 'mattermost')
+        assert service['confidential_issues_events'] is False
