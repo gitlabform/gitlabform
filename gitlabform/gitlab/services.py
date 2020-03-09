54f9743c9d7aed9849ebad3be32fb6fc3524d3bb
Greg Dubicki <gdubicki@egnyte.com>
2020-03-09 19:21:49 +0100

    Try to use 'json' requests field

--- gitlabform/gitlab/services.py
+++ gitlabform/gitlab/services.py
@@ -8,7 +8,7 @@ class GitLabServices(GitLabCore):
 
     def set_service(self, project_and_group_name, service, data):
         self._make_requests_to_api("projects/%s/services/%s", (project_and_group_name, service),
-                                   'PUT', data, expected_codes=[200, 201])
+                                   'PUT', data=None, expected_codes=[200, 201], json=data)
 
     def delete_service(self, project_and_group_name, service):
         self._make_requests_to_api("projects/%s/services/%s", (project_and_group_name, service),
