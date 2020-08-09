c105b2aa71c047ca014779ecffaaaced025cf5cd
Greg Dubicki <gdubicki@egnyte.com>
2020-08-09 10:44:40 +0200

    #126 : use Python 3.8 for Docker images and main tests

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tags=""
 version="$(cat version)"
-python_version="$(python3 --version | awk '{print $2}' | cut -d'.' -f 1-2)"
+python_version="3.8"
 for os in alpine debian
 do
     effective_version="${os}_version"
