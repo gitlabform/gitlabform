348336d606e7e0db19d093d2c647870a82f15364
jcybertech <covingtonjoshua7@gmail.com>
2020-12-15 18:50:31 -0700

    Had to revert to eliminate changes that were made to the config.yaml file. Needed to ensure naming convention for the added flag was consistent. Also added the flags within the exception block so if the flag is thrown during the process it will exit gracefully with an error message.

--- gitlabform/gitlabform/core.py
+++ gitlabform/gitlabform/core.py
@@ -29,7 +29,7 @@ class GitLabFormCore(object):
             self.set_log_level(tests=True)
             self.skip_version_check = True
             self.show_version = False
-            self.terminate_after_error = False  # Not sure if this should be false
+            self.terminate_after_error = False
         else:
             (
                 self.project_or_group,
@@ -132,7 +132,7 @@ class GitLabFormCore(object):
         parser.add_argument(
             "-t",
             "--terminate",
-            dest="terminate_error",
+            dest="terminate_after_error",
             action="store_true",
             help="Terminates the program from running after the first error",
         )
@@ -182,12 +182,6 @@ class GitLabFormCore(object):
                 version += f" (the latest is {latest_version} - please update!)"
 
         return version
-
-    def terminate_error(self, terminate_after_error):
-        if self.terminate_after_error:
-            with Exception as e:
-                print("Exiting due to: %s", e)
-                sys.exit(1)
 
     def initialize_configuration_and_gitlab(self):
 
@@ -294,6 +288,12 @@ class GitLabFormCore(object):
                 )
 
             except Exception as e:
+                if self.terminate_after_error:
+                    print(
+                        "+++ Errors occurred while processing due to '%s' ... Exiting now...",
+                        e,
+                    )
+                    sys.exit(1)
                 logging.error("+++ Error while processing '%s'", group)
                 traceback.print_exc()
 
@@ -334,6 +334,12 @@ class GitLabFormCore(object):
                 )
 
             except Exception as e:
+                if self.terminate_after_error:
+                    print(
+                        "+++ Errors occurred while processing due to '%s' ... Exiting now...",
+                        e,
+                    )
+                    sys.exit(1)
                 logging.error("+++ Error while processing '%s'", project_and_group)
                 traceback.print_exc()
 
