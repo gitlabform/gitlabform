c0aae76b0f2c0289269f4bb21602f18a158c41ba
Greg Dubicki <gdubicki@egnyte.com>
2021-01-09 19:53:58 +0100

    Add integration tests

--- dev/run_gitlab_in_docker_ci.sh
+++ dev/run_gitlab_in_docker_ci.sh
@@ -2,28 +2,10 @@
 
 set -euo pipefail
 
-# based on https://stackoverflow.com/a/28709668/2693875 and https://stackoverflow.com/a/23006365/2693875
+# no tput at all
 cecho() {
-    local color=$1
     local exp=$2
-    if ! [[ $color =~ ^[0-9]$ ]] ; then
-       case $(echo "$color" | tr '[:upper:]' '[:lower:]') in
-        bk | black) color=0 ;;
-        r | red) color=1 ;;
-        g | green) color=2 ;;
-        y | yellow) color=3 ;;
-        b | blue) color=4 ;;
-        m | magenta) color=5 ;;
-        c | cyan) color=6 ;;
-        w | white|*) color=7 ;; # white or invalid color
-       esac
-    fi
-    if [[ -z ${TERM} ]] ; then
-      TERM="dumb"
-    fi
-    tput setaf $color
     echo "$exp"
-    tput sgr0
 }
 
 cecho b "Starting GitLab..."
@@ -69,22 +51,3 @@ echo ''
 cecho b 'GitLab version:'
 curl -H "Authorization:Bearer $(cat gitlab_token.txt)" http://localhost/api/v4/version
 echo ''
-cecho b 'GitLab web UI URL (user: root, password: password)'
-echo 'http://localhost'
-echo ''
-cecho b 'Run these commands to set the env variables needed by the tests to use this GitLab instance:'
-# shellcheck disable=SC2016
-cecho g 'export GITLAB_URL=$(cat gitlab_url.txt)'
-# shellcheck disable=SC2016
-cecho g 'export GITLAB_TOKEN=$(cat gitlab_token.txt)'
-echo ''
-cecho b 'Run this command to stop GitLab container:'
-# shellcheck disable=SC2016
-cecho r 'docker stop --time=30 $(docker ps -f "ancestor=gitlab/gitlab-ee" --format "{{.ID}}")'
-echo ''
-cecho b 'To start GitLab container again run above commands again. Note that GitLab will reuse existing ./data, ./config'
-cecho b 'and ./logs dirs. To start new GitLab instance from scratch please delete them.'
-echo ''
-cecho b 'Run this to start the integration tests:'
-echo ''
-cecho y 'py.test gitlabform/gitlabform/test'
