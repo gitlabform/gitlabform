080ce8303ef5484bb794537774b87ae61d8c50e2
Greg Dubicki <gdubicki@egnyte.com>
2021-01-09 19:30:21 +0100

    Add building Docker images

--- dev/build_docker_images.sh
+++ dev/build_docker_images.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
+
+# inputs:
+#   os
+#   docker_username
+#   docker_password
+
+python_version="3.8"
 tags=""
 version="$(cat version)"
-python_version="3.8"
-for os in alpine debian
+
+effective_version="${os}_version"
+suffix="${!effective_version}"
+latest="egnyte/gitlabform:latest-${suffix}"
+
+docker pull "${latest}" || echo "no cache is available"
+docker build \
+  --build-arg PY_VERSION="${python_version}" \
+  --build-arg OS_VERSION="${!effective_version}" \
+  --file "dev/${os}.Dockerfile" \
+  --tag "${latest}" .
+tags="$tags ${latest}"
+
+docker tag "${latest}" "egnyte/gitlabform:${version}-${suffix}"
+tags="$tags egnyte/gitlabform:${version}-${suffix}"
+
+# we treat alpine image as the main one, so it gets the plain "latest" tag
+if [ "${os}" = "alpine" ]
+then
+  docker tag "${latest}" "egnyte/gitlabform:latest"
+  tags="$tags egnyte/gitlabform:latest"
+fi
+
+for image in ${tags}
 do
-    effective_version="${os}_version"
-    suffix="${!effective_version}"
-    latest="egnyte/gitlabform:latest-${suffix}"
-    docker pull "${latest}" || echo "no cache is available"
-    docker build \
-        --build-arg PY_VERSION=$python_version \
-        --build-arg OS_VERSION="${!effective_version}" \
-        --file "dev/${os}.Dockerfile" \
-        --tag "${latest}" .
-    tags="$tags ${latest}"
-
-    docker tag "${latest}" "egnyte/gitlabform:${version}-${suffix}"
-    tags="$tags egnyte/gitlabform:${version}-${suffix}"
-
-    if [ "$os" = "alpine" ]
-    then
-        docker tag "${latest}" "egnyte/gitlabform:latest"
-        tags="$tags egnyte/gitlabform:latest"
-    fi
-done
-
-for image in $tags
-do
-    if [ ! -z "${push_images}" ]
-    then
-        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-        docker push $image
-    else
-        echo "$image"
-    fi
+#  echo "$docker_password" | docker login -u "$docker_username" --password-stdin
+#  docker push "${image}"
+   echo "${image}"
 done
