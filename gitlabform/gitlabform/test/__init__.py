dd74c3b2ebb354bd694310e7d604196d19dfb041
Greg Dubicki <gdubicki@egnyte.com>
2021-01-28 22:23:53 +0100

    Add test for archiving/unarchiving projects (failing for now)

--- gitlabform/gitlabform/test/__init__.py
+++ gitlabform/gitlabform/test/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from gitlabform.gitlab import GitLab
 from gitlabform.gitlab.core import NotFoundException, UnexpectedResponseException
 
@@ -6,6 +8,27 @@ gitlab:
   # GITLAB_URL and GITLAB_TOKEN should be in the env variables
   api_version: 4
     """
+
+# automate reading files created by run_gitlab_in_docker.sh to run tests in PyCharm / IntelliJ
+# (workaround for lack of this feature: https://youtrack.jetbrains.com/issue/PY-5543 )
+
+env_vars_and_file_paths = {
+    "GITLAB_URL": "dev/gitlab_url.txt",
+    "GITLAB_TOKEN": "dev/gitlab_token.txt",
+}
+
+for env_var, file_path in env_vars_and_file_paths.items():
+    if env_var not in os.environ:
+        print(f"{env_var} not set - trying to read it from {file_path} ...")
+        if os.path.isfile(file_path):
+            try:
+                with open(file_path, "r") as file:
+                    os.environ[env_var] = file.read().replace("\n", "")
+                    print(f"{env_var} set!")
+            except Exception as e:
+                print(f"Failed to read {file_path}: {e}")
+        else:
+            print(f"{file_path} doesn't exist.")
 
 GROUP_NAME = "gitlabform_tests_group"
 
---
+++ gitlabform/gitlabform/test/test_archive_project.py
@@ -0,0 +1,121 @@
+import time
+
+import pytest
+
+from gitlabform.gitlabform import GitLabForm
+from gitlabform.gitlabform.test import (
+    create_group,
+    create_project_in_group,
+    create_readme_in_project,
+    get_gitlab,
+    GROUP_NAME,
+)
+
+PROJECT_NAME = "archive_project"
+GROUP_AND_PROJECT_NAME = GROUP_NAME + "/" + PROJECT_NAME
+
+
+@pytest.fixture(scope="function")
+def gitlab(request):
+    gl = get_gitlab()
+
+    create_group(GROUP_NAME)
+    create_project_in_group(GROUP_NAME, PROJECT_NAME)
+    create_readme_in_project(GROUP_AND_PROJECT_NAME)  # in main branch
+
+    def fin():
+        gl.delete_project(GROUP_AND_PROJECT_NAME)
+        # TODO: find some smarter way to avoid 400 when trying to create project while it is still being deleted
+        time.sleep(15)
+
+    request.addfinalizer(fin)
+    return gl  # provide fixture value
+
+
+archive_project = """
+gitlab:
+  api_version: 4
+
+project_settings:
+  gitlabform_tests_group/archive_project:
+    project:
+      archive: true
+"""
+
+try_to_edit_a_project = """
+gitlab:
+  api_version: 4
+
+group_settings:
+  gitlabform_tests_group:
+    files:
+      README.md:
+        overwrite: true
+        branches:
+          - main
+        content: |
+          Some other content that the default one
+"""
+
+unarchive_project = """
+gitlab:
+  api_version: 4
+
+project_settings:
+  gitlabform_tests_group/archive_project:
+    project:
+      archive: false
+"""
+
+
+class TestArchiveProject:
+    def test__archive_project(self, gitlab):
+        gf = GitLabForm(
+            config_string=archive_project,
+            project_or_group=GROUP_AND_PROJECT_NAME,
+        )
+        gf.main()
+
+        project = gitlab.get_project(GROUP_AND_PROJECT_NAME)
+
+        assert project["archived"] is True
+
+    def test__unarchive_project(self, gitlab):
+        gf = GitLabForm(
+            config_string=archive_project,
+            project_or_group=GROUP_AND_PROJECT_NAME,
+        )
+        gf.main()
+
+        project = gitlab.get_project(GROUP_AND_PROJECT_NAME)
+
+        assert project["archived"] is True
+
+        gf = GitLabForm(
+            config_string=unarchive_project,
+            project_or_group=GROUP_AND_PROJECT_NAME,
+        )
+        gf.main()
+
+        project = gitlab.get_project(GROUP_AND_PROJECT_NAME)
+
+        assert project["archived"] is False
+
+    def test__dont_edit_archived_project(self, gitlab):
+        gf = GitLabForm(
+            config_string=archive_project,
+            project_or_group=GROUP_AND_PROJECT_NAME,
+        )
+        gf.main()
+
+        project = gitlab.get_project(GROUP_AND_PROJECT_NAME)
+
+        assert project["archived"] is True
+
+        gf = GitLabForm(
+            config_string=try_to_edit_a_project,
+            project_or_group=GROUP_NAME,
+        )
+        gf.main()
+
+        # if we tried to edit an archived project, then we will get an exception here
