db0c1be02e7e1e63463a933ae1898f47a2d05fc7
Luis Pigueiras <luis.pigueiras@epfl.ch>
2021-04-30 11:36:38 +0200

    Do deep merges of configuration
    Fixes #197

--- gitlabform/configuration/test/test_projects_and_groups.py
+++ gitlabform/configuration/test/test_projects_and_groups.py
@@ -8,6 +8,36 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
+def configuration_with_multiple_levels_and_lists():
+    config_yaml = """
+    ---
+    projects_and_groups:
+      "*":
+        merge_requests:
+          approvals:
+            approvals_before_merge: 3
+          approvers:
+            - common_approvers
+
+      "some_group/*":
+        merge_requests:
+          approvals:
+            reset_approvals_on_push: true
+          approvers:
+            - group_approvers
+
+      "some_group/my_project":
+        merge_requests:
+          approvals:
+            reset_approvals_on_push: false
+            disable_overriding_approvers_per_merge_request: true
+          approvers:
+            - project_approvers
+    """
+    return ConfigurationProjectsAndGroups(config_string=config_yaml)
+
+
+@pytest.fixture
 def configuration_with_only_group_and_project():
     config_yaml = """
     ---
@@ -18,7 +48,7 @@ def configuration_with_only_group_and_project():
         hooks:
           a:
             foo: bar
-    
+
       some_group/some_project:
         project_settings:
           bar: foo
@@ -41,35 +71,35 @@ def configuration_with_subgroups():
         hooks:
           a:
             foo: bar
-    
+
       some_group/subgroup_level_1/*:
         project_settings:
           foo: bar2
         hooks:
           a:
             foo: bar2
-    
+
       some_group/subgroup_level_1/subgroup_level_2/*:
         project_settings:
           foo: bar3
         hooks:
           a:
             foo: bar3
-    
+
       some_group/some_project:
         project_settings:
           bar: something_else
         hooks:
           b:
             bar: something_else
-    
+
       some_group/subgroup_level_1/some_project:
           project_settings:
             bar: something_else2
           hooks:
             b:
               bar: something_else2
-    
+
       some_group/subgroup_level_1/subgroup_level_2/some_project:
           project_settings:
             bar: something_else3
@@ -148,3 +178,24 @@ def test__get_effective_config_for_project__project_from_config__level2(
 
     # project and only subgroup level 2
     assert additive__project_settings == {"foo": "bar3", "bar": "something_else3"}
+
+
+def test__get_effective_config_for_project__with_multiple_levels(
+    configuration_with_multiple_levels_and_lists,
+):
+    x = configuration_with_multiple_levels_and_lists.get_effective_config_for_project(
+        "some_group/my_project"
+    )
+
+    assert x == {
+        "merge_requests": {
+            "approvals": {
+                "approvals_before_merge": 3,
+                "reset_approvals_on_push": False,
+                "disable_overriding_approvers_per_merge_request": True,
+            },
+            "approvers": [
+                "project_approvers",
+            ],
+        }
+    }
