572967fd91a421c07822e2f52f0947712318f152
Waldek Maleska <weakcamel@gmail.com>
2021-05-08 13:22:39 +0100

    select docker tags to push based on release type

--- dev/build_docker_images.sh
+++ dev/build_docker_images.sh
@@ -4,42 +4,48 @@
 #   os
 #   docker_username
 #   docker_password
-
-python_version="3.8"
-version="$(cat version)"
+#   release_type
 
 set -ue
 
+python_version="3.8"
+version="$(< version)"
+
 effective_version="${os}_version"
 suffix="${!effective_version}"
-image_name="egnyte/gitlabform:${version}-${suffix}"
-os_latest="egnyte/gitlabform:latest-${suffix}"
+
+image_name="egnyte/gitlabform"
+default_tag="${image_name}:${version}-${suffix}"
+os_latest="${image_name}:latest-${suffix}"
+latest="${image_name}:latest"
+tags=( )
 
 docker pull "${os_latest}" || echo "no cache is available"
+
 docker build \
   --build-arg PY_VERSION="${python_version}" \
   --build-arg OS_VERSION="${!effective_version}" \
   --file "dev/${os}.Dockerfile" \
-  --tag "${image_name}" .
+  --tag "${default_tag}" .
+tags+=( "${default_tag}" )
 
-tags=( "${image_name}" )
-
-# TODO: something more clever here maybe?
-# TODO: do we need to validate the version?
-if ! [[ "${version}" =~ .+rc.+ ]]; then
-  docker tag "${image_name}" "${os_latest}" 
+if [[ "${release_type:-}" == "public" ]]; then
+  docker tag "${default_tag}" "${os_latest}" 
   tags+=( "${os_latest}" )
 
   # we treat alpine image as the main one, so it gets the plain "latest" tag
   if [[ "${os}" = "alpine" ]]; then
-    latest=egnyte/gitlabform:latest
-    docker tag "${image_name}" "${latest}"
+    docker tag "${default_tag}" "${latest}"
     tags+=( "${latest}" )
   fi
 fi
 
-echo "Images to be published: ${tags[*]}"
-for image in "${tags[@]}"; do
-  echo "$docker_password" | docker login -u "$docker_username" --password-stdin
-  docker push "${image}"
+echo "Tags to be published:"
+for tag in "${tags[@]}"; do
+  printf "  * %s\n" "${tag}"
+done
+
+echo "$docker_password" | docker login -u "$docker_username" --password-stdin
+for tag in "${tags[@]}"; do
+  docker push "${tag}"
 done
