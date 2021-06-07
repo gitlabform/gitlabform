5f1cbfe5cb2e427534fdf6aa1116eed340f9e9ef
Greg Dubicki <grzegorz.dubicki@gmail.com>
2021-06-07 18:43:48 +0200

    Test config for subgroup
    for #179.

--- gitlabform/configuration/test/test_projects_and_groups.py
+++ gitlabform/configuration/test/test_projects_and_groups.py
@@ -80,57 +80,6 @@ def configuration_with_only_group_and_project():
     return ConfigurationProjectsAndGroups(config_string=config_yaml)
 
 
-@pytest.fixture
-def configuration_with_subgroups():
-    config_yaml = """
-    ---
-    projects_and_groups:
-      some_group/*:
-        project_settings:
-          foo: bar
-        hooks:
-          a:
-            foo: bar
-
-      some_group/subgroup_level_1/*:
-        project_settings:
-          foo: bar2
-        hooks:
-          a:
-            foo: bar2
-
-      some_group/subgroup_level_1/subgroup_level_2/*:
-        project_settings:
-          foo: bar3
-        hooks:
-          a:
-            foo: bar3
-
-      some_group/some_project:
-        project_settings:
-          bar: something_else
-        hooks:
-          b:
-            bar: something_else
-
-      some_group/subgroup_level_1/some_project:
-          project_settings:
-            bar: something_else2
-          hooks:
-            b:
-              bar: something_else2
-
-      some_group/subgroup_level_1/subgroup_level_2/some_project:
-          project_settings:
-            bar: something_else3
-          hooks:
-            b:
-              bar: something_else3
-"""
-
-    return ConfigurationProjectsAndGroups(config_string=config_yaml)
-
-
 def test__get_effective_config_for_project__other_project(
     configuration_with_only_group_and_project,
 ):
@@ -172,32 +121,6 @@ def test__get_effective_config_for_project__project_from_config__additive_hooks(
         "a": {"foo": "bar"},
         "b": {"bar": "foo"},
     }  # added from both group and project level
-
-
-def test__get_effective_config_for_project__project_from_config__level1(
-    configuration_with_subgroups,
-):
-    effective_config = configuration_with_subgroups.get_effective_config_for_project(
-        "some_group/subgroup_level_1/some_project"
-    )
-
-    additive__project_settings = effective_config["project_settings"]
-
-    # project and only subgroup level 1
-    assert additive__project_settings == {"foo": "bar2", "bar": "something_else2"}
-
-
-def test__get_effective_config_for_project__project_from_config__level2(
-    configuration_with_subgroups,
-):
-    effective_config = configuration_with_subgroups.get_effective_config_for_project(
-        "some_group/subgroup_level_1/subgroup_level_2/some_project"
-    )
-
-    additive__project_settings = effective_config["project_settings"]
-
-    # project and only subgroup level 2
-    assert additive__project_settings == {"foo": "bar3", "bar": "something_else3"}
 
 
 def test__get_effective_config_for_project__with_multiple_levels(
