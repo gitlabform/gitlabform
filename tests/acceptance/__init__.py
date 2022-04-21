4bedd16f29534fe4625a935766d58f9db38099cc
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-04-21 10:45:17 +0200

    OMG this formatter is so annoying sometimes

--- tests/acceptance/__init__.py
+++ tests/acceptance/__init__.py
@@ -25,7 +25,7 @@ for env_var in env_vars_to_files.keys():
     if env_var not in os.environ:
         print(f"{env_var} not set - trying to read it from a file...")
         for up_dir_level in range(1, 4):
-            file_path = (up_dir_level * '../') + env_vars_to_files[env_var]
+            file_path = (up_dir_level * "../") + env_vars_to_files[env_var]
             print(f"Trying to read {file_path} ...")
             if os.path.isfile(file_path):
                 try:
