61300d7d94d1abc8de57dcc757ba33a7c2edc42c
Linden Kyaw <linden.kyaw@elasticpath.com>
2022-05-31 09:44:31 -0700

    Add loop to get all resource groups
    Without the loop, the config is only checking for a single resource group, but there can be multiple resource groups in a project.
    Remove the else statements because fatal exits the program.

--- gitlabform/processors/project/resource_group_processor.py
+++ gitlabform/processors/project/resource_group_processor.py
@@ -11,33 +11,33 @@ class ResourceGroupProcessor(AbstractProcessor):
         super().__init__("resource_group", gitlab)
 
     def _process_configuration(self, project_and_group: str, configuration: dict):
-        config_resource_group_name = configuration["resource_group"]["name"]
-        config_process_mode = configuration["resource_group"]["process_mode"]
+        for config_resource_group_name in configuration["resource_group"]:
+            config_process_mode = configuration["resource_group"][
+                config_resource_group_name
+            ]["process_mode"]
 
-        gitlab_resource_group = self.gitlab.get_specific_resource_group(
-            project_and_group, config_resource_group_name
-        )
+            gitlab_resource_group = self.gitlab.get_specific_resource_group(
+                project_and_group, config_resource_group_name
+            )
 
-        if gitlab_resource_group:
-            # the project is configured to use the provided resource group
-            if config_process_mode != gitlab_resource_group["process_mode"]:
-                # check the resource group process mode diff comparing config entity and gitlab entity
-                response = self.gitlab.update_resource_group(
-                    project_and_group,
-                    config_resource_group_name,
-                    {"process_mode": config_process_mode},
-                )
-                # an invalid process mode will return an empty response
-                if not response:
-                    fatal(
-                        f"process_mode does not have a valid value: {config_process_mode}\n",
-                        exit_code=EXIT_INVALID_INPUT,
+            if gitlab_resource_group:
+                # the project is configured to use the provided resource group
+                if config_process_mode != gitlab_resource_group["process_mode"]:
+                    # check the resource group process mode diff comparing config entity and gitlab entity
+                    response = self.gitlab.update_resource_group(
+                        project_and_group,
+                        config_resource_group_name,
+                        {"process_mode": config_process_mode},
                     )
-                else:
+                    # an invalid process mode will return an empty response
+                    if not response:
+                        fatal(
+                            f"process_mode does not have a valid value: {config_process_mode}\n",
+                            exit_code=EXIT_INVALID_INPUT,
+                        )
                     verbose(
                         f"Setting resource group process mode to {config_process_mode}"
                     )
-        else:
             fatal(
                 f"Project is not configured to use resource group {config_resource_group_name}\n",
                 exit_code=EXIT_INVALID_INPUT,
