36859af794c7d18a5ebcde5568790ad91dcb9444
Linden Kyaw <linden.kyaw@elasticpath.com>
2022-06-15 11:27:05 -0700

    Remove all the valid tests and change key name with key first

--- tests/unit/configuration/test_inheritance_break_validation.py
+++ tests/unit/configuration/test_inheritance_break_validation.py
@@ -8,40 +8,6 @@ logger = logging.getLogger(__name__)
 
 
 class TestInheritanceBreakValidation:
-    def test__validate_break_inheritance_flag__valid_flag_set_at_group_level(self):
-        config_yaml = """
-        ---
-        projects_and_groups:
-          some_group/*:
-            secret_variables:
-              first:
-                key: foo
-                value: bar
-        """
-
-        group_name = "some_group"
-        configuration = Configuration(config_string=config_yaml).get_group_config(
-            group_name
-        )
-        Configuration.validate_break_inheritance_flag(configuration, group_name)
-
-    def test__validate_break_inheritance_flag__valid_flag_set_at_project_level(self):
-        config_yaml = """
-        ---
-        projects_and_groups:
-          "some_group/some_project":
-            secret_variables:
-              foo: bar
-        """
-
-        group_and_project_name = "some_group/some_project"
-        configuration = Configuration(config_string=config_yaml).get_project_config(
-            group_and_project_name
-        )
-        Configuration.validate_break_inheritance_flag(
-            configuration, group_and_project_name
-        )
-
     def test__validate_break_inheritance_flag__invalid_flag_set_at_common_level(self):
         config_yaml = """
             ---
@@ -49,7 +15,7 @@ class TestInheritanceBreakValidation:
               "*":
                 inherit: false
                 secret_variables:
-                  first:
+                  secret_key:
                     key: foo
                     value: bar
             """
@@ -68,7 +34,7 @@ class TestInheritanceBreakValidation:
           some_group/*:
             secret_variables:
               inherit: false
-              first:
+              secret_key:
                 key: foo
                 value: bar
         """
@@ -89,7 +55,9 @@ class TestInheritanceBreakValidation:
           "some_group/some_project":
             secret_variables:
               inherit: false
-              foo: bar
+              secret_key:
+                key: foo
+                value: bar
         """
 
         group_and_project_name = "some_group/some_project"
@@ -102,22 +70,6 @@ class TestInheritanceBreakValidation:
             )
         assert exception.type == SystemExit
         assert exception.value.code == EXIT_INVALID_INPUT
-
-    def test__validate_break_inheritance_flag__valid_flag_set_at_subgroup_level(self):
-        config_yaml = """
-        ---
-        projects_and_groups:
-          some_group/subgroup/*:
-            group_members:
-              my-user:
-                access_level: 10
-        """
-
-        subgroup_name = "some_group/subgroup"
-        configuration = Configuration(config_string=config_yaml).get_group_config(
-            subgroup_name
-        )
-        Configuration.validate_break_inheritance_flag(configuration, subgroup_name)
 
     def test__validate_break_inheritance_flag__invalid_flag_set_at_subgroup_level(self):
         config_yaml = """
