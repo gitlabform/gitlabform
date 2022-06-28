7fc85cc6279203f24bfadf7eb16bcbd45ca9bde6
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-06-28 18:28:09 +0200

    Remove support for deprecated services syntax

--- gitlabform/processors/project/services_processor.py
+++ gitlabform/processors/project/services_processor.py
@@ -1,5 +1,4 @@
 from cli_ui import debug as verbose
-from cli_ui import warning
 
 from gitlabform.gitlab import GitLab
 from gitlabform.processors.abstract_processor import AbstractProcessor
@@ -15,19 +14,6 @@ class ServicesProcessor(AbstractProcessor):
                 verbose(f"Deleting service: {service}")
                 self.gitlab.delete_service(project_and_group, service)
             else:
-
-                if (
-                    "recreate" in configuration["services"][service]
-                    and configuration["services"][service]["recreate"]
-                ):
-                    # support from this configuration key has been added in v1.13.4
-                    # we will remove it here to avoid passing it to the GitLab API
-                    warning(
-                        f"Ignoring deprecated 'recreate' field in the '{service}' service config. "
-                        "Please remove it from the config file permanently as this workaround is not "
-                        "needed anymore."
-                    )
-                    del configuration["services"][service]["recreate"]
 
                 verbose(f"Setting service: {service}")
                 self.gitlab.set_service(
