a5f38910bc15245f8e965ddd85c061101ffe3740
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-10-04 13:59:58 +0100

    Implement enforce: true support

--- gitlabform/processors/multiple_entities_processor.py
+++ gitlabform/processors/multiple_entities_processor.py
@@ -44,74 +44,104 @@ class MultipleEntitiesProcessor(AbstractProcessor, metaclass=abc.ABCMeta):
             return
 
         entities_in_configuration = configuration[self.configuration_name]
+        if "enforce" in entities_in_configuration:
+            enforce = True
+            del entities_in_configuration["enforce"]
+        else:
+            enforce = False
 
         # TODO: move/convert this to a configuration validation phase
         self._find_duplicates(project_or_group, entities_in_configuration)
 
-        entities_in_gitlab = self.list_method(project_or_group)
+        entities_in_gitlab = {}
+        i = 1
+        for entity_in_gitlab in self.list_method(project_or_group):
+            entities_in_gitlab[str(i)] = entity_in_gitlab
+            i += 1
+
         debug(f"{self.configuration_name} BEFORE: ^^^")
 
-        # 1. group entities into 3 groups:
-        # a) only in configuration,
+        # group entities into 3 groups:
+        # a) only in gitlab,
         # b) in both configuration and gitlab (matching defining keys),
-        # c) only in gitlab,
+        # c) only in configuration,
 
-        # 2. if "enforce", then delete c)
-        # TODO: implement this.
+        entities_only_in_gitlab = {
+            entity_name: entity_config
+            for (entity_name, entity_config) in entities_in_gitlab.items()
+            if not self._is_in(entity_config, entities_in_configuration)
+        }
 
-        # 3. update b), if needed (or delete them if marked as "delete")
+        entities_in_both = {
+            entity_name: entity_config
+            for (entity_name, entity_config) in entities_in_configuration.items()
+            if self._is_in(entity_config, entities_in_gitlab)
+        }
 
-        # 4. add a) (or do nothing if marked as "delete")
+        entities_only_in_configuration = {
+            entity_name: entity_config
+            for (entity_name, entity_config) in entities_in_configuration.items()
+            if not self._is_in(entity_config, entities_in_gitlab)
+        }
 
-        for entity_name, entity_config in entities_in_configuration.items():
+        # if "enforce", then delete a)
 
-            entity_in_gitlab = self._is_in_gitlab(entity_config, entities_in_gitlab)
-            if entity_in_gitlab:
-                if entity_config.get("delete", False):
-                    self._validate_required_to_delete(
-                        project_or_group, entity_name, entity_config
-                    )
+        if enforce:
+            for entity_name, entity_config in entities_only_in_gitlab.items():
+                # no need to validate if we have what's needed to delete as we got the entities from gitlab
+                verbose(
+                    f"Deleting entity no {entity_name} of {self.configuration_name} in {project_or_group} "
+                    f" as it's not in config and enforce is set to true."
+                )
+                self.delete_method(project_or_group, entity_config)
+
+        # update b), if needed (or delete them if marked as "delete")
+
+        for entity_name, entity_config in entities_in_both.items():
+
+            entity_in_gitlab = self._is_in(entity_config, entities_in_gitlab)
+            if entity_config.get("delete", False):
+                self._validate_required_to_delete(
+                    project_or_group, entity_name, entity_config
+                )
+                verbose(
+                    f"Deleting {entity_name} of {self.configuration_name} in {project_or_group}"
+                )
+                self.delete_method(project_or_group, entity_in_gitlab)
+            elif self._needs_update(entity_in_gitlab, entity_config):
+                self._validate_required_to_create_or_update(
+                    project_or_group, entity_name, entity_config
+                )
+                if self.edit_method:
                     verbose(
-                        f"Deleting {entity_name} of {self.configuration_name} in {project_or_group}"
+                        f"Editing {entity_name} of {self.configuration_name} in {project_or_group}"
+                    )
+                    self.edit_method(project_or_group, entity_in_gitlab, entity_config)
+                    debug(f"{self.configuration_name} AFTER: ^^^")
+                else:
+                    verbose(
+                        f"Recreating {entity_name} of {self.configuration_name} in {project_or_group}"
                     )
                     self.delete_method(project_or_group, entity_in_gitlab)
-                elif self._needs_update(entity_in_gitlab, entity_config):
-                    self._validate_required_to_create_or_update(
-                        project_or_group, entity_name, entity_config
-                    )
-                    if self.edit_method:
-                        verbose(
-                            f"Editing {entity_name} of {self.configuration_name} in {project_or_group}"
-                        )
-                        self.edit_method(
-                            project_or_group, entity_in_gitlab, entity_config
-                        )
-                        debug(f"{self.configuration_name} AFTER: ^^^")
-                    else:
-                        verbose(
-                            f"Recreating {entity_name} of {self.configuration_name} in {project_or_group}"
-                        )
-                        self.delete_method(project_or_group, entity_in_gitlab)
-                        self.add_method(project_or_group, entity_config)
-                        debug(f"{self.configuration_name} AFTER: ^^^")
-                else:
-                    verbose(
-                        f"{entity_name} of {self.configuration_name} in {project_or_group} doesn't need an update."
-                    )
-            else:
-                if entity_config.get("delete", False):
-                    verbose(
-                        f"{entity_name} of {self.configuration_name} in {project_or_group} already doesn't exist."
-                    )
-                else:
-                    self._validate_required_to_create_or_update(
-                        project_or_group, entity_name, entity_config
-                    )
-                    verbose(
-                        f"Adding {entity_name} of {self.configuration_name} in {project_or_group}"
-                    )
                     self.add_method(project_or_group, entity_config)
                     debug(f"{self.configuration_name} AFTER: ^^^")
+            else:
+                verbose(
+                    f"{entity_name} of {self.configuration_name} in {project_or_group} doesn't need an update."
+                )
+
+        # add c) (or do nothing if marked as "delete")
+
+        for entity_name, entity_config in entities_only_in_configuration.items():
+
+            self._validate_required_to_create_or_update(
+                project_or_group, entity_name, entity_config
+            )
+            verbose(
+                f"Adding {entity_name} of {self.configuration_name} in {project_or_group}"
+            )
+            self.add_method(project_or_group, entity_config)
+            debug(f"{self.configuration_name} AFTER: ^^^")
 
     def _find_duplicates(self, project_or_group: str, entities_in_configuration: dict):
         for first_key, first_value in entities_in_configuration.items():
@@ -145,11 +175,9 @@ class MultipleEntitiesProcessor(AbstractProcessor, metaclass=abc.ABCMeta):
                 exit_code=EXIT_INVALID_INPUT,
             )
 
-    def _is_in_gitlab(
-        self, entity_in_configuration: dict, entities_in_gitlab: List[dict]
-    ):
-        for entity_in_gitlab in entities_in_gitlab:
-            if self.defining.matches(entity_in_gitlab, entity_in_configuration):
-                return entity_in_gitlab
+    def _is_in(self, entity: dict, dict_of_entities: dict):
+        for entity_name, entity_config in dict_of_entities.items():
+            if self.defining.matches(entity, entity_config):
+                return entity_config
 
         return False
