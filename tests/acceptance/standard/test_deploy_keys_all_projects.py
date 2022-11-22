1e307fc9df97edd777b5e07f078538cdc7fa967f
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-11-22 11:36:12 +0000

    Reproduce bug #19 (thx jpiron)

--- tests/acceptance/standard/test_deploy_keys_all_projects.py
+++ tests/acceptance/standard/test_deploy_keys_all_projects.py
@@ -23,3 +23,25 @@ class TestDeployKeysAllProjects:
 
         deploy_keys2 = gitlab.get_deploy_keys(f"{group}/{other_project}")
         assert len(deploy_keys2) == 1
+
+    def test__deploy_key_with_spaces_in_comment_to_all_projects(
+        self, gitlab, group, project, other_project, public_ssh_key
+    ):
+        public_ssh_key = f"{public_ssh_key} this is a comment with spaces"
+
+        deploy_key_to_all_projects = f"""
+        projects_and_groups:
+          "*":
+            deploy_keys:
+              foobar:
+                key: {public_ssh_key}
+                title: common_key
+                can_push: false
+        """
+        run_gitlabform(deploy_key_to_all_projects, group)
+
+        deploy_keys1 = gitlab.get_deploy_keys(f"{group}/{project}")
+        assert len(deploy_keys1) == 1
+
+        deploy_keys2 = gitlab.get_deploy_keys(f"{group}/{other_project}")
+        assert len(deploy_keys2) == 1
