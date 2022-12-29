e09f8f6c4cf960f4fbd04f9aa215f0a06ae49e69
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-12-29 22:32:34 +0100

    More tests for converting legacy single merge request approval rule

--- tests/unit/test_transform.py
+++ tests/unit/test_transform.py
@@ -305,50 +305,125 @@ class TestImplicitNameTransformer(TestCase):
 #
 
 
-def test__config__with_merge_request_approvals__single_approval():
-    config_before = f"""
-    projects_and_groups:
-      foobar/*:
-        merge_requests:
-          approvals:
-            approvals_before_merge: 2
-            reset_approvals_on_push: true
-            disable_overriding_approvers_per_merge_request: true
-          approvers:
-            - user1
-            - user2
-          approver_groups:
-            - my-group
-            - my-group1/subgroup
-            - my-group2/subgroup/subsubgroup
-          remove_other_approval_rules: true
-    """
-    config_before = Configuration(config_string=config_before)
+class TestMergeRequestApprovalsTransformer(TestCase):
+    def test__config__with_merge_request_approvals__many_users_and_groups_and_remove(
+        self,
+    ):
+        config_before = f"""
+        projects_and_groups:
+          foobar/*:
+            merge_requests:
+              approvals:
+                approvals_before_merge: 2
+                reset_approvals_on_push: true
+                disable_overriding_approvers_per_merge_request: true
+              approvers:
+                - user1
+                - user2
+              approver_groups:
+                - my-group
+                - my-group1/subgroup
+                - my-group2/subgroup/subsubgroup
+              remove_other_approval_rules: true
+        """
+        config_before = Configuration(config_string=config_before)
 
-    transformer = MergeRequestApprovalsTransformer(MagicMock(GitLab))
-    transformer.transform(config_before)
-    config_before = ez_yaml.to_string(obj=config_before.config, options={})
+        transformer = MergeRequestApprovalsTransformer(MagicMock(GitLab))
+        transformer.transform(config_before)
+        config_before = ez_yaml.to_string(obj=config_before.config, options={})
 
-    config_after = f"""
-    projects_and_groups:
-      foobar/*:
-        merge_requests_approval_rules:
-          legacy:
-            approvals_required: 2
-            name: {APPROVAL_RULE_NAME}
-            users:
-              - user1
-              - user2
-            groups:
-              - my-group
-              - my-group1/subgroup
-              - my-group2/subgroup/subsubgroup
-          enforce: true
-        merge_requests_approvals:
-          reset_approvals_on_push: true
-          disable_overriding_approvers_per_merge_request: true
-    """
-    config_after = Configuration(config_string=config_after)
-    config_after = ez_yaml.to_string(obj=config_after.config, options={})
+        config_after = f"""
+        projects_and_groups:
+          foobar/*:
+            merge_requests_approval_rules:
+              legacy:
+                approvals_required: 2
+                name: {APPROVAL_RULE_NAME}
+                users:
+                  - user1
+                  - user2
+                groups:
+                  - my-group
+                  - my-group1/subgroup
+                  - my-group2/subgroup/subsubgroup
+              enforce: true
+            merge_requests_approvals:
+              reset_approvals_on_push: true
+              disable_overriding_approvers_per_merge_request: true
+        """
+        config_after = Configuration(config_string=config_after)
+        config_after = ez_yaml.to_string(obj=config_after.config, options={})
 
-    assert config_before == config_after
+        assert config_before == config_after
+
+    def test__config__with_merge_request_approvals__single_user_no_remove(self):
+        config_before = f"""
+        projects_and_groups:
+          '*':
+            merge_requests:
+              approvals:
+                approvals_before_merge: 2
+                reset_approvals_on_push: false
+                disable_overriding_approvers_per_merge_request: false
+              approvers:
+                - user1
+        """
+        config_before = Configuration(config_string=config_before)
+
+        transformer = MergeRequestApprovalsTransformer(MagicMock(GitLab))
+        transformer.transform(config_before)
+        config_before = ez_yaml.to_string(obj=config_before.config, options={})
+
+        config_after = f"""
+        projects_and_groups:
+          '*':
+            merge_requests_approval_rules:
+              legacy:
+                approvals_required: 2
+                name: {APPROVAL_RULE_NAME}
+                users:
+                  - user1
+            merge_requests_approvals:
+              reset_approvals_on_push: false
+              disable_overriding_approvers_per_merge_request: false
+        """
+        config_after = Configuration(config_string=config_after)
+        config_after = ez_yaml.to_string(obj=config_after.config, options={})
+
+        assert config_before == config_after
+
+    def test__config__with_merge_request_approvals__single_group_no_remove(self):
+        config_before = f"""
+        projects_and_groups:
+          'foo/bar':
+            merge_requests:
+              approvals:
+                approvals_before_merge: 1
+                reset_approvals_on_push: false
+                disable_overriding_approvers_per_merge_request: true
+              approver_groups:
+                - my-group2
+        """
+        config_before = Configuration(config_string=config_before)
+
+        transformer = MergeRequestApprovalsTransformer(MagicMock(GitLab))
+        transformer.transform(config_before)
+        config_before = ez_yaml.to_string(obj=config_before.config, options={})
+
+        config_after = f"""
+        projects_and_groups:
+          'foo/bar':
+            merge_requests_approval_rules:
+              legacy:
+                approvals_required: 1
+                name: {APPROVAL_RULE_NAME}
+                groups:
+                  - my-group2
+            merge_requests_approvals:
+              reset_approvals_on_push: false
+              disable_overriding_approvers_per_merge_request: true
+        """
+        config_after = Configuration(config_string=config_after)
+        config_after = ez_yaml.to_string(obj=config_after.config, options={})
+
+        assert config_before == config_after
