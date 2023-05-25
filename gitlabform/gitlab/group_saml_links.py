57b8a0526cd6386b9c6b669233752c6d813bc633
Pat Turner <pattu@nhsbsa.nhs.uk>
2023-05-25 20:38:51 +0100

    formatting

--- gitlabform/gitlab/group_saml_links.py
+++ gitlabform/gitlab/group_saml_links.py
@@ -3,7 +3,6 @@ from gitlabform.gitlab.groups import GitLabGroups
 
 
 class GitLabGroupSAMLLinks(GitLabGroups):
-    
     def get_saml_group_links(self, group):
         group_id = self.get_group_id_case_insensitive(group)
         result = self._make_requests_to_api(
@@ -41,7 +40,7 @@ class GitLabGroupSAMLLinks(GitLabGroups):
         group_id = self.get_group_id_case_insensitive(group)
         data["id"] = group_id
         saml_group_name = data["saml_group_name"]
-        
+
         # 404 means that the SAML group link is already removed, so let's accept it for idempotency
         self._make_requests_to_api(
             "groups/%s/saml_group_links/%s",
