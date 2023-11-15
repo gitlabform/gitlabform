cfc08d2dcaeb2485beb2cd54ada8a787f2f9dac2
TigreModerata <pdritter@gmail.com>
2023-11-15 15:50:59 +0100

    added check equality before hook update
    avoid unnecessary call if config hook same as gl hook.

--- gitlabform/processors/project/hooks_processor.py
+++ gitlabform/processors/project/hooks_processor.py
@@ -18,7 +18,8 @@ class HooksProcessor(AbstractProcessor):
         hooks_list: RESTObjectList | List[RESTObject] = project.hooks.list()
 
         for hook in sorted(configuration["hooks"]):
-            hook_id = next((h.id for h in hooks_list if h.url == hook), None)
+            gitlab_hook = next((h for h in hooks_list if h.url == hook), None)
+            hook_id = gitlab_hook.id if gitlab_hook else None
             if configuration.get("hooks|" + hook + "|delete"):
                 if hook_id:
                     debug("Deleting hook '%s'", hook)
@@ -28,13 +29,13 @@ class HooksProcessor(AbstractProcessor):
             else:
                 hook_config = {"url": hook}
                 hook_config.update(configuration["hooks"][hook])
-                if hook_id:
+                if hook_id and gitlab_hook.asdict() != hook_config:
                     debug("Changing existing hook '%s'", hook)
                     changed_hook: Dict[str, Any] = project.hooks.update(
                         hook_id, hook_config
                     )
                     debug("Changed hook to '%s'", changed_hook)
-                else:
+                elif not hook_id:
                     debug("Creating hook '%s'", hook)
                     created_hook: RESTObject = project.hooks.create(hook_config)
                     debug("Created hook '%s'", created_hook)
