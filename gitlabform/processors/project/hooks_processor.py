f50a04484c6a6995ac7b5cebb2fd8fd93e61b983
amimas <aver.mimas@gmail.com>
2024-03-01 23:45:51 -0500

    refactor: use existing logic from super class to check if hook update is needed

--- gitlabform/processors/project/hooks_processor.py
+++ gitlabform/processors/project/hooks_processor.py
@@ -48,7 +48,7 @@ class HooksProcessor(AbstractProcessor):
 
             # Processing existing hook updates
             gl_hook: dict = hook_in_gitlab.asdict() if hook_in_gitlab else {}
-            if self.is_hook_config_different(gl_hook, hook_config):
+            if self._needs_update(gl_hook, hook_config):
                 debug(
                     f"The hook '{hook}' config is different from what's in gitlab OR it contains a token"
                 )
@@ -68,39 +68,3 @@ class HooksProcessor(AbstractProcessor):
                         f"Deleting hook '{gh.url}' currently setup in the project but it is not in the configuration and enforce is enabled"
                     )
                     project.hooks.delete(gh.id)
-
-    def is_hook_config_different(
-        self, config_in_gitlab: dict, config_in_gitlabform: dict
-    ):
-        """
-        Compare two dictionary representing a webhook configuration and determine
-        if they are different.
-
-        GitLab's webhook data does not contain "token" as it is considered a secret.
-        If GitLabForm config for a webhook contains a "token", the difference cannot
-        be validated. In this case, the config is assumed to be different. Otherwise,
-        the config data will be compared and determined if they are different.
-
-        Args:
-            config_in_gitlab (dict): hook configuration in gitlab
-            config_in_gitlabform (dict): hook configuration in gitlabform
-
-        Returns:
-            boolean: True if the two configs are different. Otherwise False.
-        """
-        if "token" in config_in_gitlabform:
-            debug(
-                f"The hook '{config_in_gitlabform['url']}' config includes a token. Diff between config vs gitlab cannot be confirmed."
-            )
-            return True
-
-        diffs = (
-            map(
-                lambda k: config_in_gitlabform[k] != config_in_gitlab[k],
-                config_in_gitlabform.keys(),
-            )
-            if config_in_gitlab
-            else iter(())
-        )
-
-        return True if any(diffs) else False
