df494ce9373b03012cf060ec697d9a112ba2c4be
Tim Knight <tim.knight1@engineering.digital.dwp.gov.uk>
2024-05-02 14:08:58 +0100

    test: validate setting of array field
    Signed-off-by: Tim Knight <tim.knight1@engineering.digital.dwp.gov.uk>

--- tests/acceptance/standard/test_application_settings.py
+++ tests/acceptance/standard/test_application_settings.py
@@ -30,6 +30,36 @@ class TestApplicationSettings:
         updated_settings.two_factor_grace_period = 0
         updated_settings.save()
 
+    def test__can_set_asset_proxy_allowlist_array_field_into_application_settings(
+        self, gl, project
+    ):
+        initial_settings = gl.settings.get()
+        assert initial_settings is not None
+        assert initial_settings.asset_proxy_allowlist is not None
+
+        config_app_settings = f"""
+          application:
+            settings:
+              asset_proxy_allowlist: ["example.com", "*.example.com", "your-instance.com"]
+          projects_and_groups:
+            placeholder:
+          """
+
+        run_gitlabform(config_app_settings, project)
+
+        updated_settings = gl.settings.get()
+        assert updated_settings is not None
+        assert updated_settings.asset_proxy_allowlist == [
+            "example.com",
+            "*.example.com",
+            "your-instance.com",
+            "localhost",
+        ]
+
+        # Tear down
+        updated_settings.asset_proxy_allowlist = initial_settings.asset_proxy_allowlist
+        updated_settings.save()
+
     def test__can_detect_when_no_changes_required_to_application_settings(
         self, gl, project, caplog
     ):
