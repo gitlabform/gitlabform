d889db1da8058e60790c8a52207c1d0dfeaa116b
Mahadevan Karthi <mahadevan.karthi@engineering.digital.dwp.gov.uk>
2024-05-03 16:20:54 +0100

    feat: use python lib for group settings

--- gitlabform/processors/group/group_settings_processor.py
+++ gitlabform/processors/group/group_settings_processor.py
@@ -1,12 +1,30 @@
+from logging import info, debug
+from typing import Dict
+
 from gitlabform.gitlab import GitLab
-from gitlabform.processors.single_entity_processor import SingleEntityProcessor
+from gitlab.v4.objects.groups import Group
+from gitlabform.processors.abstract_processor import AbstractProcessor
 
 
-class GroupSettingsProcessor(SingleEntityProcessor):
+class GroupSettingsProcessor(AbstractProcessor):
     def __init__(self, gitlab: GitLab):
-        super().__init__(
-            "group_settings",
-            gitlab,
-            get_method_name="get_group_settings",
-            edit_method_name="put_group_settings",
-        )
+        super().__init__("group_settings", gitlab)
+
+    def _process_configuration(self, group: str, configuration: Dict):
+        configured_group_settings = configuration.get("group_settings", {})
+
+        gitlab_group: Group = self.gl.get_group_by_path_cached(group)
+
+        if self._needs_update(gitlab_group.asdict(), configured_group_settings):
+            info(f"Updating group settings for group {gitlab_group.name}")
+            self.update_group_settings(gitlab_group, configured_group_settings)
+        else:
+            debug("No update needed for Group Settings")
+
+    @staticmethod
+    def update_group_settings(gitlab_group: Group, group_settings_config: dict):
+        for key in group_settings_config:
+            value = group_settings_config[key]
+            debug(f"Updating setting {key} to value {value}")
+            gitlab_group.__setattr__(key, value)
+            gitlab_group.save()
