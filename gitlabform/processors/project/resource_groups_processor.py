beabdfd9ae0a50aa8c28e0ce81d5689702855fd6
amimas <aver.mimas@gmail.com>
2024-10-13 09:28:55 -0400

    fix: add logic to catch update failure with test case

--- gitlabform/processors/project/resource_groups_processor.py
+++ gitlabform/processors/project/resource_groups_processor.py
@@ -4,8 +4,9 @@ from cli_ui import warning
 from gitlabform.gitlab import GitLab
 from gitlabform.processors.abstract_processor import AbstractProcessor
 
-from gitlab import GitlabGetError
+from gitlab import GitlabGetError, GitlabUpdateError
 from gitlab.v4.objects import Project, ProjectResourceGroup
+
 
 class ResourceGroupsProcessor(AbstractProcessor):
     def __init__(self, gitlab: GitLab):
@@ -22,9 +23,13 @@ class ResourceGroupsProcessor(AbstractProcessor):
         project: Project = self.gl.get_project_by_path_cached(project_and_group)
 
         for config_resource_group_name in sorted(configured_resource_groups):
-            config_resource_group_process_mode = configured_resource_groups[config_resource_group_name]["process_mode"]
+            config_resource_group_process_mode = configured_resource_groups[
+                config_resource_group_name
+            ]["process_mode"]
             try:
-                resource_group_in_gitlab: ProjectResourceGroup = project.resource_groups.get(config_resource_group_name)
+                resource_group_in_gitlab: ProjectResourceGroup = (
+                    project.resource_groups.get(config_resource_group_name)
+                )
             except GitlabGetError:
                 message = (
                     f"Project is not configured to use resource group: {config_resource_group_name}.\n"
@@ -39,7 +44,20 @@ class ResourceGroupsProcessor(AbstractProcessor):
                     warning(message)
                     continue
 
-            if resource_group_in_gitlab and resource_group_in_gitlab.process_mode != config_resource_group_process_mode:
-                verbose("Updating process mode of '%s' resource group to '%s'", config_resource_group_name, config_resource_group_process_mode)
-                resource_group_in_gitlab.process_mode = config_resource_group_process_mode
-                resource_group_in_gitlab.save()
+            if (
+                resource_group_in_gitlab
+                and resource_group_in_gitlab.process_mode
+                != config_resource_group_process_mode
+            ):
+                verbose(
+                    f"Updating process mode of '{config_resource_group_name}' resource group to '{config_resource_group_process_mode}'"
+                )
+
+                try:
+                    resource_group_in_gitlab.process_mode = (
+                        config_resource_group_process_mode
+                    )
+                    resource_group_in_gitlab.save()
+                except GitlabUpdateError as error:
+                    warning(f"Resource group update failed. Error: '{error}'")
+                    raise GitlabUpdateError
