b06df0ab96478c1a9bda643cfd79595399935dd0
szEvEz <szivos.john@gmail.com>
2025-01-07 10:38:14 +0100

    fix: change log output back to debug

--- gitlabform/processors/group/group_push_rules_processor.py
+++ gitlabform/processors/group/group_push_rules_processor.py
@@ -21,7 +21,7 @@ class GroupPushRulesProcessor(AbstractProcessor):
             existing_push_rules = gitlab_group.pushrules.get()
         except GitlabGetError as e:
             if e.response_code == 404:
-                print(
+                debug(
                     f"No existing push rules for {gitlab_group.name}, creating new push rules."
                 )
                 self.create_group_push_rules(gitlab_group, configured_group_push_rules)
@@ -30,12 +30,12 @@ class GroupPushRulesProcessor(AbstractProcessor):
         if self._needs_update(
             existing_push_rules.asdict(), configured_group_push_rules
         ):
-            print(f"Updating group push rules for group {gitlab_group.name}")
+            debug(f"Updating group push rules for group {gitlab_group.name}")
             self.update_group_push_rules(
                 existing_push_rules, configured_group_push_rules
             )
         else:
-            print("No update needed for Group Push Rules")
+            debug("No update needed for Group Push Rules")
 
     @staticmethod
     def update_group_push_rules(push_rules, configured_group_push_rules: dict):
