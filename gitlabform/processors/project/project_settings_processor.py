fd2ce1d3061205041f17117e4c2022fd7518cb83
amimas <aver.mimas@gmail.com>
2025-01-19 18:08:55 -0500

    refactor: update 'project_settings_processor' to use python-gitlab

--- gitlabform/processors/project/project_settings_processor.py
+++ gitlabform/processors/project/project_settings_processor.py
@@ -1,12 +1,28 @@
+from logging import debug
+
 from gitlabform.gitlab import GitLab
-from gitlabform.processors.single_entity_processor import SingleEntityProcessor
+from gitlabform.processors.abstract_processor import AbstractProcessor
+
+from gitlab.v4.objects import Project
 
 
-class ProjectSettingsProcessor(SingleEntityProcessor):
+class ProjectSettingsProcessor(AbstractProcessor):
     def __init__(self, gitlab: GitLab):
-        super().__init__(
-            "project_settings",
-            gitlab,
-            get_method_name="get_project_settings",
-            edit_method_name="put_project_settings",
-        )
+        super().__init__("project_settings", gitlab)
+
+    def _process_configuration(self, project_path: str, configuration: dict):
+        debug("Processing project settings...")
+        project: Project = self.gl.get_project_by_path_cached(project_path)
+
+        project_settings_in_config = configuration.get("project_settings", {})
+        project_settings_in_gitlab = project.asdict()
+
+        if self._needs_update(project_settings_in_gitlab, project_settings_in_config):
+            debug("Updating project settings")
+            for key, value in project_settings_in_config.items():
+                debug(f"Updating setting {key} to value {value}")
+                setattr(project, key, value)
+
+            project.save()
+        else:
+            debug("No update needed for project settings")
