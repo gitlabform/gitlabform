afd9af631dbca0e52feca700e8d12a726feedebe
amimas <aver.mimas@gmail.com>
2025-06-08 15:49:27 -0400

    Revert "test: add more check for debugging CI failure"
    This reverts commit b83a2868a7ec697c9ce479b85311f7f405b58915.

--- tests/acceptance/standard/test_group_variables.py
+++ tests/acceptance/standard/test_group_variables.py
@@ -1,4 +1,8 @@
 from logging import warning, fatal
+<<<<<<< HEAD
+=======
+from time import sleep
+>>>>>>> parent of b83a2868 (test: add more check for debugging CI failure)
 from typing import Any, Dict, List, Union
 from io import StringIO
 
