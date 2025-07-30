29dc7828b6eda7ce453984f982a42ccc048ab68b
Tim Knight <tim.knight1@engineering.digital.dwp.gov.uk>
2025-07-30 09:35:21 +0100

    feat: implement "exclude-sections" CLI parameter (#1016)
    * feat: implement "exclude-sections" CLI parameter
    
    - resolves #1005
    
    * chore: make os and es mutually exclusive
    
    * docs: improved warning
    
    Co-authored-by: amimas <aver.mimas@gmail.com>
    
    ---------
    
    Co-authored-by: Tim Knight <tim.knight@opencastsoftware.com>
    Co-authored-by: amimas <aver.mimas@gmail.com>

--- gitlabform/processors/__init__.py
+++ gitlabform/processors/__init__.py
@@ -25,16 +25,20 @@ class AbstractProcessors(ABC):
         dry_run: bool,
         diff_only_changed: bool,
         effective_configuration: EffectiveConfigurationFile,
-        only_sections: List[str],
+        only_sections: List[str] | str,
+        exclude_sections: List[str],
     ):
         for processor in self.processors:
-            if only_sections == "all" or processor.configuration_name in only_sections:
-                processor.process(
-                    entity_reference,
-                    configuration,
-                    dry_run,
-                    diff_only_changed,
-                    effective_configuration,
-                )
+            if processor.configuration_name not in exclude_sections:
+                if only_sections == "all" or processor.configuration_name in only_sections:
+                    processor.process(
+                        entity_reference,
+                        configuration,
+                        dry_run,
+                        diff_only_changed,
+                        effective_configuration,
+                    )
+                else:
+                    verbose(f"Skipping section '{processor.configuration_name}' - not in --only-sections list.")
             else:
-                verbose(f"Skipping section '{processor.configuration_name}' - not in --only-sections list.")
+                verbose(f"Excluding section '{processor.configuration_name}'.")
