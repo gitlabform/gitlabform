817e38d4f457bdf2e326c2b495dc8ae5129a14bc
Thomas Di Cizerone <thomas.cizeron@hotmail.fr>
2026-01-25 20:31:14 +0100

    🗨️ Add docstring for acceptance test

--- tests/acceptance/standard/test_project_settings.py
+++ tests/acceptance/standard/test_project_settings.py
@@ -7,6 +7,9 @@ from tests.acceptance import run_gitlabform
 
 class TestProjectSettings:
     def test__builds_for_private_projects(self, gl: Gitlab, project: Project) -> None:
+        """
+        Test that we can change visibility from private to internal
+        """
         assert project.visibility == "private"
 
         config_builds_for_private_projects: str = f"""
@@ -45,6 +48,9 @@ class TestProjectSettings:
         assert "topicB" in project_topics
 
     def test__edit_project_settings_topics_default(self, gl: Gitlab, project: Project) -> None:
+        """
+        Test that the topics are replaced when topics are specified in the config
+        """
         project.topics = ["topicA", "topicB"]
         project.save()
 
@@ -67,6 +73,9 @@ class TestProjectSettings:
         assert "topicD" in project_topics
 
     def test__edit_project_settings_topics_keep_existing_true(self, gl: Gitlab, project: Project) -> None:
+        """
+        Test that the existing topics are kept when keep_existing is true
+        """
         project.topics = ["topicA", "topicB"]
         project.save()
 
@@ -92,6 +101,9 @@ class TestProjectSettings:
         assert "topicD" in project_topics
 
     def test__edit_project_settings_topics_keep_existing_false(self, gl: Gitlab, project: Project) -> None:
+        """
+        Test that the existing topics are not kept when keep_existing is false
+        """
         project.topics = ["topicA", "topicB"]
         project.save()
 
@@ -115,6 +127,9 @@ class TestProjectSettings:
         assert "topicD" in project_topics
 
     def test__edit_project_settings_topics_delete(self, gl: Gitlab, project: Project) -> None:
+        """
+        Test that the existing topics are deleted when delete is true
+        """
         project.topics = ["topicA", "topicB"]
         project.save()
 
