6ae712a2a1c08b41758c67f0be244f14333ea765
Thomas Di Cizerone <66677623+thomascizeron@users.noreply.github.com>
2026-02-01 17:37:29 +0100

    refactor: migrate project security settings to python-gitlab (#1152)

--- tests/acceptance/ultimate/test_project_security_settings.py
+++ tests/acceptance/ultimate/test_project_security_settings.py
@@ -1,5 +1,7 @@
 from tests.acceptance import run_gitlabform
-from gitlabform.gitlab.project_security_settings import GitlabProjectSecuritySettings
+from gitlabform.processors.project.project_security_settings import (
+    ProjectSecuritySettingsProcessor,
+)
 
 
 class TestProjectSecuritySettings:
@@ -14,8 +16,7 @@ class TestProjectSecuritySettings:
 
         run_gitlabform(config, project)
 
-        updated_project_security_settings = GitlabProjectSecuritySettings(
-            config_string=config
-        ).get_project_security_settings(project.path_with_namespace)
+        processor = ProjectSecuritySettingsProcessor(gitlab=gl)
+        updated_project_security_settings = processor.get_project_security_settings(project.path_with_namespace)
 
         assert updated_project_security_settings["pre_receive_secret_detection_enabled"] is True
