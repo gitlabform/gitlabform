70490462af888732471cf415a5b01031c2883111
Rick Brouwer <rickbrouwer@gmail.com>
2026-07-13 20:13:59 +0200

    refactor: drop dead code exposed by python-gitlab migration
    Signed-off-by: Rick Brouwer <rickbrouwer@gmail.com>

--- gitlabform/gitlab/core.py
+++ gitlabform/gitlab/core.py
@@ -158,11 +158,6 @@ class GitLabCore:
         return int(group["id"])
 
     @functools.lru_cache()
-    def _get_protected_branch_id(self, project_and_group_name, branch) -> int:
-        branch = self._make_requests_to_api("projects/%s/protected_branches/%s", (project_and_group_name, branch))
-        return int(branch["id"])
-
-    @functools.lru_cache()
     def _get_project_id(self, project_and_group):
         # This is a NEW workaround for https://github.com/gitlabhq/gitlabhq/issues/8290
         result = self.get_project(project_and_group)
@@ -316,14 +311,6 @@ class TestRequestFailedException(Exception):
 
 
 class NotFoundException(Exception):
-    pass
-
-
-class TimeoutWaitingForDeletion(Exception):
-    pass
-
-
-class InvalidParametersException(Exception):
     pass
 
 
